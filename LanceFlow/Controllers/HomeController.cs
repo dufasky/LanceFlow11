10a59df61b27d46b4674962d53bba507a953d5be
dufasky <52201132+dufasky@users.noreply.github.com>
2020-05-17 18:22:04 +0500

    Добавление информации во вкладку "О программе"
    asp-action="Privacy"

--- LanceFlow/Controllers/HomeController.cs
+++ LanceFlow/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace LanceFlow.Controllers
 
             return View(model);
         }
+        public IActionResult Privacy()
+        {
+
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
